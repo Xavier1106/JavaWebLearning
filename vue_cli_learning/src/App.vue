e2c3c050ed3382f99e2bb58df598ffaf293ded41
Xavier1106 <1546868258@qq.com>
2023-04-09 14:12:30 +0800

    done learning Element components

--- vue_cli_learning/src/App.vue
+++ vue_cli_learning/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <ElementView></ElementView>
   </div>
 </template>
 
 <script>
+import ElementView from "@/views/element/ElementView";
+
 export default {
+  components: {
+    ElementView
+  },
+
   data() {
     return {
       message: "Hello Vue!"
